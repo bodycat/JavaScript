a4ee9b9fb8df60aa9ad954da3164a5bdfc75d3b0
Khizri Tataev <bodycat@gmail.com>
2019-03-01 15:25:47 +0300

    Add files via upload

--- hw05/hw_01.js
+++ hw05/hw_01.js
@@ -1,27 +1,42 @@
-function chess() {
-    for (var i = 1; i < 11; i++) {
+var table = document.createElement("table");
 
-        var tr = document.createElement("tr");
+for (var i = 1; i < 11; i++) {
 
-        for (var j = 1; j < 11; j++){
-            var td = document.createElement("td");
-            if (i==1 || j==1) {
-                td.className = "transparent";
-            }
-            else if (i==10 || j==10) {
-                td.className = "transparent"
-            }        
-            else if (i%2 == j%2) {
-                td.className = "whitesmoke";            
-            } else {
-                td.className = "peru";
-              }
-            tr.appendChild(td);
+    var tr = document.createElement("tr");
+
+    for (var j = 1; j < 11; j++){
+        var td = document.createElement("td");
+        if (i==1 || j==1) {
+            td.className = "transparent"
         }
-
-        table.appendChild(tr);
+        else if (i==10 || j==10) {
+            td.className = "transparent"
+        }        
+        else if (i%2 == j%2) {
+            td.className = "whitesmoke";            
+        } else {
+            td.className = "peru";
+          }
+        tr.appendChild(td);
     }
 
-    document.body.appendChild(table);
+    table.appendChild(tr);
 }
-chess();
+
+document.body.appendChild(table);
+
+// var table = document.createElement("table");
+// for (var i = 1; i < 9; i++) {
+//     var tr = document.createElement('tr');
+//     for (var j = 1; j < 9; j++) {
+//         var td = document.createElement('td');
+//         if (i%2 == j%2) {
+//             td.className = "white";
+//         } else {
+//             td.className = "gray";
+//         }
+//         tr.appendChild(td);
+//     }
+//     table.appendChild(tr);
+// }
+// document.body.appendChild(table);
